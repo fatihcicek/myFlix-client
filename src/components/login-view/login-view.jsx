7a72e3d0fa24a11aa33cfb0c37edc7fe027683f7
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-09-25 19:15:27 +0300

    Update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -39,9 +39,5 @@ export function LoginView(props) {
 }
 
 LoginView.propTypes = {
-  user: PropTypes.shape({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
-  }),
-  onLoggedIn: PropTypes.func.isRequired,
-}; 
+  onLoggedIn: PropTypes.func.isRequired
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,86 +7,114 @@ import MovieCard from '../movie-card/movie-card';
 import MovieView from '../movie-view/movie-view';
 
 
-export class MainView extends React.Component {
-
+class MainView extends React.Component {
   constructor() {
     super();
+    //initial state is set to null
     this.state = {
       movies: [],
       selectedMovie: null,
       user: null,
       register: null,
-    }
+    };
   }
-  componentDidMount(){
-    axios.get('https://myflixdb-fatihcicek.herokuapp.com/movies')
-      .then(response => {
+
+  componentDidMount() {
+    axios
+      .get("https://cinemapp-backend.herokuapp.com/movies")
+      .then((response) => {
         this.setState({
           movies: response.data,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }
-  setSelectedMovie(newSelectedMovie) {
+
+  /*When a movie is clicked, this function is invoked and updates 
+  the state of the `selectedMovie` *property to that movie*/
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie,
     });
   }
-/* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
-onLoggedIn(user) {
-  this.setState({
-    user,
-  });
-}
+  onRegistration(register) {
+    this.setState({
+      register,
+    });
+  }
 
-onRegister(register) {
-  this.setState({
-    register,
-  });
+  /* When a user successfully logs in, this function updates the
+   `user` property in state to that *particular user*/
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  getGenres() {
+    axios
+      .get("https://cinemapp-backend.herokuapp.com/genres")
+      .then((response) => {
+        this.setState({
+          genres: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
-    const { movies, selectedMovie, user, register } = this.state;
+    const { movies, selectedMovie, user, register, genres } = this.state;
 
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user)
-      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
-
+    /* If there is not registered user, the RegisterView is rendered. If there is a user registered,
+     the user details are *passed as a prop to the LoginView*/
     if (!register)
       return (
         <RegistrationView
-          onRegister={(register) => this.onRegister(register)}
+          onRegistration={(register) => this.onRegistration(register)}
         />
       );
+
+    /* If there is no user, the LoginView is rendered. If there is a user logged in,
+     the user details are *passed as a prop to the LoginView*/
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {selectedMovie ? (
-          <MovieView
-            movie={selectedMovie}
-            onBackClick={(newSelectedMovie) => {
-              this.setSelectedMovie(newSelectedMovie);
-            }}
-          />
-        ) : (
-          movies.map((movie) => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
+        {
+          /*If the state of `selectedMovie` is not null, that selected movie 
+          will be returned otherwise, all *movies will be returned*/
+          selectedMovie ? (
+            <MovieView
+              movie={selectedMovie}
+              onBackClick={(newSelectedMovie) => {
                 this.setSelectedMovie(newSelectedMovie);
               }}
             />
-          ))
-        )}
+          ) : (
+            movies.map((movie) => (
+              <MovieCard
+                key={movie._id}
+                movieData={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  this.setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            ))
+          )
+        }
       </div>
     );
   }
 }
 
-// so we  get rid of the curly braces when importing MainView in index.jsx
-export default MainView;  
+export default MainView;
